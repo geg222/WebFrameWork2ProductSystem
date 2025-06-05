68ef9512556d7f30bbb554a80be95cc0019f9843
geg222 <gl021414@naver.com>
2025-06-05 09:37:47 +0900

    fix: pom.xml 에러 수정

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
@@ -1,69 +1,31 @@
 package kr.ac.hansung.cse.hellospringdatajpa.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-
-
-@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 @Entity
 @Table(name = "product")
 public class Product {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String name;
-        private String brand;
-        private String madeIn;
-        private double price;
+    private String name;
+    private String brand;
+    private String madeIn;
+    private double price;
 
-        public Product() {}
-
-        public Product(String name, String brand, String madeIn, double price) {
-            this.name = name;
-            this.brand = brand;
-            this.madeIn = madeIn;
-            this.price = price;
-        }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Product(String name, String brand, String madeIn, double price) {
         this.name = name;
-    }
-
-    public String getMadeIn() {
-        return madeIn;
-    }
-
-    public void setMadeIn(String madeIn) {
-        this.madeIn = madeIn;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
         this.brand = brand;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
+        this.madeIn = madeIn;
         this.price = price;
     }
 }
